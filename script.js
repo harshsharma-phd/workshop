68c7d5b1e69da57c52de50aa905d173109300ba6
harshsharma-phd <harsh.sharma@iitb.ac.in>
2024-10-20 16:16:19 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+});
