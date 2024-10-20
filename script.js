651ce8326c5bb4a0ab7a5a66bf2743c0cc8cc5f5
harshsharma-phd <harsh.sharma@iitb.ac.in>
2024-10-20 16:42:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.dropdown-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        const dropdownContent = this.nextElementSibling;
-        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-    });
-});
